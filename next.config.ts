c3138f2d1daa41414c12171320cff22216b064d0
fajrulalam <fajrulalam01@gmail.com>
2025-02-27 14:40:33 +0700

    removed ES line

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   domains: ["localhost"], // Update this with your production domain if needed
-  //   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-  //   imageSizes: [16, 32, 64, 96, 128, 256, 384],
-  //   formats: ["image/webp"], // Use WebP for better compression
-  //   minimumCacheTTL: 60 * 60 * 24 * 7, // Cache for 7 days
-  // },
-  // experimental: {
-  //   optimizeCss: true, // Optional: optimize CSS
-  // },
+  eslint: {
+    // Disable ESLint during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
